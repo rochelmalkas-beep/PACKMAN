84781fe7d18857cd29d7c36751e61588fc1dbcfd
iskaMoallem <y0556729@gmail.com>
2026-01-21 01:31:03 +0200

    שמע

--- פוריקט 2/javaScript/soundConstructer.js
+++ פוריקט 2/javaScript/soundConstructer.js
@@ -1,12 +1,9 @@
-// קובץ: javaScript/SoundData.js
 class SoundData {
     constructor() {
-        // טוענים את כל הסאונדים לזיכרון מראש
-        this.munch = new Audio('../sound/eating.mp3'); 
-        this.death = new Audio('../sound/die.mp3');
-        this.win = new Audio('../sound/win.mp3');
+this.munch = new Audio('../assets/sound/eating.mp3');
+        this.death = new Audio('../assets/sound/die.mp3');
+        this.win = new Audio('../assets/sound/win.mp3');
         
-        // הגדרות אופציונליות (למשל ווליום חלש יותר לאכילה)
         this.munch.volume = 0.5;
     }
 }
